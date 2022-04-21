fc5df6335536313e34ad7d22da07adca695cc20c
ridebus82 <ridebus82@nate.com>
2022-04-21 19:40:12 +0900

    gunicorn install update

--- accountapp/views.py
+++ accountapp/views.py
@@ -26,7 +26,7 @@ has_ownership = [account_ownership_required, login_required]
 class AccountCreateView(CreateView):
     model = User
     form_class = UserCreationForm
-    success_url = reverse_lazy('accountapp:hello_world')
+    success_url = reverse_lazy('home')
     template_name = 'accountapp/create.html'
 
 
@@ -47,7 +47,7 @@ class AccountDetailView(DetailView, MultipleObjectMixin):
 class AccountUpdateView(UpdateView):
     model = User
     form_class = AccountUpdateForm
-    success_url = reverse_lazy('accountapp:hello_world')
+    success_url = reverse_lazy('home')
     template_name = 'accountapp/update.html'
 
 
--- profileapp/views.py
+++ profileapp/views.py
@@ -14,7 +14,6 @@ class ProfileCreateView(CreateView):
     model = Profile
     context_object_name = 'target_profile'
     form_class = ProfileCreationForm
-    # success_url = reverse_lazy('accountapp:hello_world')
     template_name = 'profileapp/create.html'
 
     def form_valid(self, form):
@@ -33,7 +32,6 @@ class ProfileUpdateView(UpdateView):
     model = Profile
     context_object_name = 'target_profile'
     form_class = ProfileCreationForm
-    # success_url = reverse_lazy('accountapp:hello_world')
     template_name = 'profileapp/update.html'
     print(Profile.user)
 
