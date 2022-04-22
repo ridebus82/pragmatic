e10b6ffed599cc92615cb30027f29aa57549970d
ridebus82 <ridebus82@nate.com>
2022-04-23 01:48:33 +0900

    django course 60 commit

---
+++ pragmatic/settings/deploy.py
@@ -0,0 +1,39 @@
+
+
+# Take environment variables from .env file
+import os, environ
+from .base import *
+
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
+
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = env('SECRET_KEY')
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = False
+
+ALLOWED_HOSTS = ['*']
+
+# Database
+# https://docs.djangoproject.com/en/4.0/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'django',
+        'USER': 'django',
+        'PASSWORD': '112313na',
+        'HOST': 'mariadb',
+        'PORT': '3306',
+    }
+}
